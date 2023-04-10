693e03419c7ce2c78203655d8abd5210825002e2
Lakshminarayanan AS <lucksmy@gmail.com>
2023-04-10 14:52:18 +0530

    Users Class code commit

--- lambda/LambdaEg.java
+++ lambda/LambdaEg.java
@@ -1,5 +1,7 @@
 package lambda;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class LambdaEg 
 {
@@ -22,6 +24,24 @@ public class LambdaEg
         th1.start();
 
         new Thread(new Task()::connect).start();
+
+        System.out.println("users example");
+        
+        User u1 = new User(11,"Nilima","nilima@gmail.com"); User u2 = new
+        User(12,"Praveen","prav@gmail.com"); User u3 = new
+        User(13,"Aarika","arika@gmail.com");
+        
+        
+        List<User> li = new ArrayList<User>(); li.add(u1); li.add(u2); li.add(u3);
+        System.out.println("User List :" + li);
+        
+        
+        Iterator<User> i = li.iterator(); while (i.hasNext()) 
+        {
+        System.out.println(i.next());
+        
+        }
+        li.forEach(lis->System.out.println(lis));
     }
 }
 
@@ -40,4 +60,54 @@ class Task {
     {
         System.out.println("inside Connect");
     }
+}
+
+
+class User 
+{
+	private int uid;
+	private String uname;
+	private String email;
+
+	public User() {
+		super();
+		
+	}
+
+	public User(int uid, String uname, String email) {
+		super();
+		this.uid = uid;
+		this.uname = uname;
+		this.email = email;
+	}
+
+	public int getUid() {
+		return uid;
+	}
+
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+
+	public String getUname() {
+		return uname;
+	}
+
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "User [uid=" + uid + ", uname=" + uname + ", email=" + email + "]";
+	}
+
 }
