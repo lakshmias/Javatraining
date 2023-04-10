09a2fb933a1877c261c5cc5cddc4d300e96e7a0a
Lakshminarayanan AS <lucksmy@gmail.com>
2023-04-10 14:38:39 +0530

    Lambda expressions checkins

---
+++ lambda/LambdaEg.java
@@ -0,0 +1,42 @@
+package lambda;
+import java.util.ArrayList;
+
+public class LambdaEg 
+{
+    public static void main(String[] args) {
+        //Java 7
+        // Task task1=new Task();
+        // task1.connect();
+
+        // Runnable r=new MyTaskThread();
+        // Thread theread =new Thread(r);
+        // theread.start();
+
+        //Jav 8
+        Thread th1=new Thread(
+        ()->{
+            Task task=new  Task();
+            task.connect();
+        }
+        );
+
+        new Thread(new Task()::connect).start();
+    }
+}
+
+class MyTaskThread implements Runnable 
+{
+    @Override
+    public void run()
+    {
+        Task t=new Task();
+        t.connect();
+    }
+}
+
+class Task {
+    public void connect()
+    {
+        System.out.println("inside Connect");
+    }
+}
