342a21392ab8949880db776eaf561684102f89e7
Lakshminarayanan AS <lucksmy@gmail.com>
2023-04-11 16:03:56 +0530

    remove class files and add new java files

---
+++ 11 April/LongRunTest.java
@@ -0,0 +1,24 @@
+import java.math.BigInteger;
+import java.util.Random;
+import java.util.concurrent.CompletableFuture;
+
+public class LongRunTest {
+    public static void main(String[] args) {
+        System.out.println("Before call");
+        LongRunTest l = new LongRunTest();
+        CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
+            return l.randomBigNumber().toString();
+        });
+
+        future.thenAccept(System.out::println);
+       
+        System.out.println("After call");
+        future.join();
+    }
+
+    public BigInteger randomBigNumber() {
+        BigInteger veryBig = new BigInteger(3500, new Random());
+        veryBig.nextProbablePrime();
+        return veryBig;
+    }
+}
