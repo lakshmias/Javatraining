d06a199cb6c14be17709ad865d4c27796574bb70
Lakshminarayanan AS <lucksmy@gmail.com>
2023-04-12 15:23:26 +0530

    Employee insert into oracle db commit

---
+++ 12 April/Employee.java
@@ -0,0 +1,155 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.io.Serializable;
+import java.util.Comparator;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Employee implements Serializable,Comparable<Employee>{
+
+    public static void main(String[] args) {
+        System.out.println("Starting db ops");
+        EmployeeDaoImpl oEmployeeDaoImpl=new EmployeeDaoImpl();
+        Employee oEmployee=new Employee();
+        oEmployee.eid=101;
+        oEmployee.ename="Success";
+        oEmployeeDaoImpl.insertEmployee(oEmployee);
+    }
+	private int eid;
+	private String ename;
+
+	public int getEid() {
+		return eid;
+	}
+
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+
+	public String getEname() {
+		return ename;
+	}
+
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [eid=" + eid + ", ename=" + ename + "]";
+	}
+
+	@Override
+	public int compareTo(Employee e) {
+		
+		return this.eid - e.eid;
+	
+	}
+	
+	public static Comparator<Employee> NameComparator = new Comparator<Employee>() {
+
+		@Override
+		public int compare(Employee e1, Employee e2) {
+			//return e1.getEid() -e2.getEid();
+			return e1.getEname().compareTo(e2.getEname());
+		}
+	
+	
+	};
+
+}
+interface EmployeeDao {
+	
+	public void insertEmployee(Employee emp);
+	public void showEmployee();
+	public void updateEmployee(Employee emp);
+	public void deleteEmployee(int eid);
+
+}
+
+class EmployeeDaoImpl implements EmployeeDao{
+
+	Connection con;
+	PreparedStatement ps;
+	Statement stmt;
+	ResultSet rs;
+	
+	
+	@Override
+	public void insertEmployee(Employee emp) {
+	     
+		try {
+			con  = MyDBConnection.getDbConnection();
+			ps=con.prepareStatement("insert into EMP (EMPNO,ENAME) values(?,?)");
+			
+			ps.setInt(1, emp.getEid());
+			ps.setString(2, emp.getEname());
+			int noofrows = ps.executeUpdate();
+			System.out.println(noofrows  + " got inserted... ");
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+	@Override
+	public void showEmployee() {	
+		try {
+			con  = MyDBConnection.getDbConnection();
+			stmt= con.createStatement();
+			rs = stmt.executeQuery("select * from dalemp");
+			while(rs.next()) {
+				System.out.println(rs.getInt(1) + " -- " + rs.getString(2));
+				
+			}
+			
+			
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+	@Override
+	public void updateEmployee(Employee emp) {
+	
+		
+	}
+
+	@Override
+	public void deleteEmployee(int eid) {
+	
+		
+	}
+
+}
+
+class MyDBConnection {
+	static Connection con;
+
+	public static Connection getDbConnection() {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "sys as sysdba", "pass123");
+            System.out.println("connected");
+		} catch (ClassNotFoundException e) {
+			
+			e.printStackTrace();
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		
+		return con;
+	}
+
+	
+}
