907a7c1da73e8a79b4b079d6f53fc7cb4e9a4dc2
Lakshminarayanan AS <lucksmy@gmail.com>
2023-04-27 17:26:34 +0530

    maven first project commit

---
+++ 27 April/myfirstspr/src/main/java/com/dal/MainClass.java
@@ -0,0 +1,14 @@
+package com.dal;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ApplicationContext context=new ClassPathXmlApplicationContext("welcome.xml");
+		Welcome wel=(Welcome)context.getBean("welc");
+		System.out.println(wel.sayHi());
+	}
+
+}
