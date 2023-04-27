d84a6519994875180b610b59409a4b50ab250c52
Lakshminarayanan AS <lucksmy@gmail.com>
2023-04-27 14:12:40 +0530

    porject documents commit

---
+++ 26 April/infiniteLoop.java
@@ -0,0 +1,8 @@
+public class infiniteLoop {
+    public static void main(String[] args) {
+        for(;;)
+        {
+            infiniteLoop i=new infiniteLoop();
+        }
+    }
+}
---
+++ 26 April/lists.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.event.ObjectChangeListener;
+
+public class lists {
+    List<Integer> lstInts;
+    public static void main(String[] args) {
+        lists objLst=new lists();
+        objLst.lstInts=new ArrayList<>();
+        objLst.lstInts.add(1);
+        objLst.lstInts.add(2);
+        System.out.println("inside");
+        objLst.lstInts.forEach(item->{System.out.println(item*item);}
+        System.out.println(objLst.lstInts.get(0)->System.out.println(objLst.lstInts.get(0)););
+
+        );
+    }
+}
